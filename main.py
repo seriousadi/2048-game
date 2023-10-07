412ee973feb3c006f62e2cc9355f1727239f35bc
Aditya Kumar <aaz22hsns@gmail.com>
2023-10-07 15:57:53 +0530

    Make left swipe effect work

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from random import randint
 
-grid = [[0, 2, 2, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 9],
+grid = [[8, 2, 2, 0],
+        [0, 1, 1, 0],
+        [0, 3, 0, 9],
         [6, 0, 8, 0]]
 
+
+def move_left(row):
+    def move_elements(row):
+        for n in range(len(row)):
+            if row[n] == 0:
+                if row[n] == 0:
+                    row.pop(n)
+                    row.append(0)
+                for n in range(len(row[n:])):
+                    if row[n] == 0:
+                        row.pop(n)
+                        row.append(0)
+
+    move_elements(row)
+    k = 0
+    while k < len(row) - 1:
+        p = k + 1
+        if row[k] == row[p] and row[k] != 0:
+            row[k] = row[p] * 2
+            row[p] = 0
+            move_elements(row)
+        k += 1
+
+
+for n in grid:
+   move_left(n)
 
 def spawn_num():
     grid[randint(0, 3)][randint(0, 3)] = 2
@@ -13,10 +39,10 @@ def spawn_num():
 def print_grid():
     for n in grid:
         print(*n)
-
-
-spawn_num()
 print_grid()
+
+# spawn_num()
+# print_grid()
 
 game_on = False
 
