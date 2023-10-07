709b14ee2ed4ca87bbeae1c176da9d65a8f80b30
Aditya Kumar <aaz22hsns@gmail.com>
2023-10-07 16:16:10 +0530

    make left swipe work

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from random import randint
 
 grid = [[8, 2, 2, 0],
         [0, 1, 1, 0],
-        [0, 3, 0, 9],
-        [6, 0, 8, 0]]
+        [3, 3, 3, 9],
+        [8, 0, 8, 0]]
 
 
 def move_left(row):
     def move_elements(row):
         for n in range(len(row)):
             if row[n] == 0:
-                if row[n] == 0:
-                    row.pop(n)
-                    row.append(0)
+                row.pop(n)
+                row.append(0)
                 for n in range(len(row[n:])):
                     if row[n] == 0:
                         row.pop(n)
@@ -29,9 +28,30 @@ def move_left(row):
         k += 1
 
 
-for n in grid:
-   move_left(n)
+def move_right(row):
+    def move_elements(row):
+        for n in range(len(row)-1, -1, -1):
+            if row[n] == 0:
+                row.pop(n)
+                row.insert(0,0)
+                for n in range(len(row[:n])):
+                    if row[n] == 0:
+                        row.pop(n)
+                        row.insert(0,0)
 
+    move_elements(row)
+    k = len(row)-1
+    while k > 1:
+        p = k - 1
+        if row[k] == row[p] and row[k] != 0:
+            row[k] = row[p] * 2
+            row[p] = 0
+            move_elements(row)
+        k -= 1
+
+
+for n in grid:
+   move_right(n)
 def spawn_num():
     grid[randint(0, 3)][randint(0, 3)] = 2
 
@@ -39,7 +59,7 @@ def spawn_num():
 def print_grid():
     for n in grid:
         print(*n)
-print_grid()
+
 
 # spawn_num()
 # print_grid()
