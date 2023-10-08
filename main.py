ecab8aee4d1803edf686ea67a5ef602edbcace97
Aditya Kumar <aaz22hsns@gmail.com>
2023-10-08 16:09:19 +0530

    make a way to use up swipe and down swipe

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from random import randint
 
-grid = [[8, 2, 2, 0],
-        [0, 1, 1, 0],
-        [3, 3, 3, 9],
-        [8, 0, 8, 0]]
+grid = [[8, 0, 9, 0],
+        [0, 1, 9, 9],
+        [0, 1, 9, 9],
+        [8, 8, 8, 8]]
 
 
 def move_left(row):
@@ -28,19 +28,26 @@ def move_left(row):
         k += 1
 
 
+grid_column = 0
+for n in range(len(grid)):
+    grid_column = [grid[0][n], grid[1][n], grid[2][n], grid[3][n]]
+    move_left(grid_column)
+    grid[0][n], grid[1][n], grid[2][n], grid[3][n] = grid_column[0], grid_column[1], grid_column[2], grid_column[3]
+
+
 def move_right(row):
     def move_elements(row):
-        for n in range(len(row)-1, -1, -1):
+        for n in range(len(row) - 1, -1, -1):
             if row[n] == 0:
                 row.pop(n)
-                row.insert(0,0)
+                row.insert(0, 0)
                 for n in range(len(row[:n])):
                     if row[n] == 0:
                         row.pop(n)
-                        row.insert(0,0)
+                        row.insert(0, 0)
 
     move_elements(row)
-    k = len(row)-1
+    k = len(row) - 1
     while k > 1:
         p = k - 1
         if row[k] == row[p] and row[k] != 0:
@@ -50,8 +57,10 @@ def move_right(row):
         k -= 1
 
 
-for n in grid:
-   move_right(n)
+# for n in grid:
+#    move_right(n)
+#
+
 def spawn_num():
     grid[randint(0, 3)][randint(0, 3)] = 2
 
@@ -60,6 +69,8 @@ def print_grid():
     for n in grid:
         print(*n)
 
+
+# print_grid()
 
 # spawn_num()
 # print_grid()
